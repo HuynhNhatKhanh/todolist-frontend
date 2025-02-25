fc9c65be81bfe9bb6262b668498fcd718fac7c21
Khanh <khanhhuynh28082000@gmail.com>
2025-02-25 09:56:43 +0700

    feat: [Todo-list] Update config

--- src/services/axios/axiosInstance.ts
+++ src/services/axios/axiosInstance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+
 const axiosInstance = axios.create({
-  baseURL: "https://54.211.223.242",
+  baseURL: baseURL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
